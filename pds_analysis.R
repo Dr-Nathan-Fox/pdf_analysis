1c692c8c99b4415dcd4f747ef1cd72cdbb55a8ff
nfox29 <nf2g13@soton.ac.uk>
2020-07-07 11:22:18 +0100

    - added descriptions to each line of code - added section to extract the names of papers that contain a given key word

--- pds_analysis.R
+++ pds_analysis.R
@@ -1,9 +1,15 @@
+#### library needed packages ####
+
 # install.packages("pdftools")
 library(pdftools)
 # install.packages("tm")
 library(tm)
 # install.packages("ggplot2")
 library(ggplot2)
+# install.packages("dplyr")
+library(dplyr)
+
+#### read in pdf files and store as term document matrix ####
 
 #create object with pdf names
 files <- list.files(path = ".\\papers", pattern = "pdf$")
@@ -24,11 +30,23 @@ pdf_tdm <- TermDocumentMatrix(corp,
                                           removeNumbers = TRUE, #remove numers
                                           bounds = list(global = c(3, Inf)))) #keep words that appear more than 3 times
 
+
+#### create dataframe with frequence of key words per paper ####
+
 #list of keywords to find (this search will be determined at a later date)
 keywords <- c("ethic", "social")
 
-#find the number of times the ket words appearences in each pdf
+#find the number of times the keyword appearences in each pdf
 keyword_per_paper <- as.matrix(pdf_tdm[keywords,])
+
+#### summarise which papers contain a given keyword ####
+
+#find which papers contain a chosen keyword (e.g. here I choose ethic)
+papers_containing <- t(keyword_per_paper) #reshape df
+papers_containing <- subset(papers_containing, papers_containing[, "ethic"]  > 0) #filter papers where word is mentioned 1 or more times
+papers_containing <- data.frame(paper = row.names(papers_containing)) #store papers as a df col 
+
+#### calculate and plot the total number of papers that contain each key word ####
 
 #calculate how many papers mention each keyword
 keyword_usage <- data.frame(paper_count = rowSums(keyword_per_paper > 0))
